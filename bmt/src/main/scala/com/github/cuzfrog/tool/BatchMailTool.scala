ee161a24193a64778bc72466d7f8e4f5ee62f22c
Cause chung <cuzfrog@139.com>
2016-08-08 17:32:51 +0800

    add more option to bmt

--- bmt/src/main/scala/com/github/cuzfrog/tool/BatchMailTool.scala
+++ bmt/src/main/scala/com/github/cuzfrog/tool/BatchMailTool.scala
@@ -37,25 +37,34 @@ object BatchMailTool extends App {
       p("a simple cmd tool for sending batch text emails.")
       println("Use a csv file to define emails and a config.xml file to define mail server and authentication info.")
       println("----------------------")
-      println("args            (* means indispensable)explanations")
-      println("-mailsPath:     *the path of csv file that contains mail contents.")
-      println("-configPath:     the path of config.xml file that contains maila configuration. default: ./")
-      println("-encoding:       mail content encoding. default:GBK")
-      println("-toHead:         the csv first line(head)'s field name for mail To. default: to")
-      println("-subjectHead:    the csv first line(head)'s field name for mail Subject. default: subject")
-      println("-textHead:       the csv first line(head)'s field name for mail Text content. default: text")
+      println("args              explanations(* means indispensable)")
+      println("-mailsPath:      *the path of csv file that contains mail contents.")
+      println("-configPath:      the path of config.xml file that contains maila configuration. default: ./")
+      println("-obfuscateConfig: if true, config file will be obfuscated after first mail sending. default: true")
+      println("-encoding:        mail content encoding. default:GBK")
+      println("-toHead:          the csv first line(head)'s field name for mail To. default: to")
+      println("-subjectHead:     the csv first line(head)'s field name for mail Subject. default: subject")
+      println("-textHead:        the csv first line(head)'s field name for mail Text content. default: text")
       println("----------------------")
-      println("commands         explanations")
-      println("send:            run this application, must provide arguments.")
-      println("-version:        show version.")
-      println("-help:           print this help.")
+      println("commands          explanations")
+      println("send              run this application, must provide arguments.")
+      println("-version          show version.")
+      println("-help             print this help.")
     case "-version" => p(version)
     case _ => "Bad arguments, use -help see instructions."
   }
 
   def p(s: Any) = println(s"Batch mail tool: $s")
 
-  private lazy val maila = Maila.newInstance(configPath, willObfuscate = true, TRANSFORM_KEYS.map(_.getBytes("utf8")))
+  private lazy val willObfuscate = _args.find(_.startsWith("-obfuscateConfig:")) match {
+    case Some(pathArg) => pathArg.split(":").last match {
+      case "true" => true
+      case "false" => false
+    }
+    case None => true
+  }
+
+  private lazy val maila = Maila.newInstance(configPath, willObfuscate, TRANSFORM_KEYS.map(_.getBytes("utf8")))
 
   private lazy val mails = {
     val encoding = _args.find(_.startsWith("-encoding:")) match {
