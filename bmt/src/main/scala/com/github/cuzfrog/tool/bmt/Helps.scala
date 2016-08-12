847d8ca3c63bc88b038bef24519c220fddde3cff
Cause chung <cuzfrog@139.com>
2016-08-12 15:31:17 +0800

    refine helps printing

--- bmt/src/main/scala/com/github/cuzfrog/tool/bmt/Helps.scala
+++ bmt/src/main/scala/com/github/cuzfrog/tool/bmt/Helps.scala
@@ -16,7 +16,9 @@ private[bmt] object Helps {
     println("commands          explanations")
     println("send              run this application. ")
     println("     example:")
-    println("     >bmt send -mailsPath:./mails.csv")
+    println("     >bmt send -mailsPath:./mails.csv            this will ask password later.")
+    println("     >bmt send -mailsPath:./mails.csv -pw:myPass")
+    println("     >bmt send -mailsPath:./mails.csv -key:someKeyString")
     println("test              run this application to check mails, not sending.")
     println("-version          show version.")
     println("-help             print this help.")
@@ -26,6 +28,7 @@ private[bmt] object Helps {
     println("encrypt           encrypt password with (random) key and print it and its key. ")
     println("     example:")
     println("     >bmt encrypt -pw:myPassword -key:w0j9j3pc1lht5c6b")
-    println("     >bmt encrypt -pw:myPassword                        this will use a random key.")
+    println("     >bmt encrypt -pw:myPassword                 this will use a random key.")
+    println("Goto (github.com/cuzfrog/maila) see more.")
   }
 }
