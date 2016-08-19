01710142b136ce5ed06f0a16c0a5540125fe2481
Cause chung <cuzfrog@139.com>
2016-08-19 18:00:33 +0800

    dealing with prompt test

--- src/test/scala/com/github/cuzfrog/TempTest.scala
+++ src/test/scala/com/github/cuzfrog/TempTest.scala
@@ -3,21 +3,12 @@ package com.github.cuzfrog
 import java.time.format.DateTimeFormatter
 import java.util.concurrent.atomic.AtomicReference
 
+import com.typesafe.config.ConfigValueFactory
+
 /**
   * Created by cuz on 2016-08-16.
   */
 object TempTest extends App {
 
-  sealed trait A
-  case class A1() extends A{
-    println("A1 is initialized.")
-  }
-  case object A2 extends A
 
-  val ref= new AtomicReference[A]()
-
-  ref.lazySet(new A1)
-  ref.set(A2)
-
-  println(ref.get())
 }
